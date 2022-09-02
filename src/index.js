77c825c11abd9ca0995244152eb99741f8886627
Vika Borel <vika.borel@gmail.com>
2022-09-02 13:39:30 -0600

    1sr unrevised version

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const axisElementsNumber = 3;
-const lrDiagonal = Array.from([...Array(axisElementsNumber).keys()], x => x * (axisElementsNumber + 1));
-const rlDiagonal = Array.from([...Array(axisElementsNumber).keys()], x => (axisElementsNumber - 1) * (x + 1));
-
 function Cell(props) {
   return (
-      <button className={`cell ${ props.value }`} onClick={props.onClick}>
+      <button className={`cell ${ props.value }`} onClick={ props.onClick }>
       </button>
   );
 }
@@ -16,17 +12,46 @@ function Cell(props) {
 function WinningMessage(props) {
   return (
       <div className={`winning-message ${ props.showWinningMessage }`}>
-        <button>Restart</button>
+        <div>{`The winner is ${ props.winner === 'x' ? 'X' : 'O' }`}</div>
+        <button onClick={ props.onClick }>Restart</button>
       </div>
   );
 }
 
 class Board extends React.Component {
+  renderCell(index) {
+    return (
+        <Cell
+            value={this.props.cells[index]}
+            onClick={() => this.props.onClick(index)}
+        />
+    );
+  }
+
+  render() {
+    return (
+        <div className={`board ${this.props.cellType}`}>
+          {this.renderCell(0)}
+          {this.renderCell(1)}
+          {this.renderCell(2)}
+          {this.renderCell(3)}
+          {this.renderCell(4)}
+          {this.renderCell(5)}
+          {this.renderCell(6)}
+          {this.renderCell(7)}
+          {this.renderCell(8)}
+        </div>
+    );
+  }
+}
+
+class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       cells: Array(9).fill(null),
       cellType: 'x',
+      winner: null,
       showWinningMessage: null,
     };
   }
@@ -46,94 +71,37 @@ class Board extends React.Component {
       cellType: this.state.cellType === 'x' ? 'circle' : 'x',
     });
 
-    if (this.isWinner(index, currentCellType, cells)) {
-      console.log(`${currentCellType} is a winner!`);
-
-      // сюда нужно поставить ресет ячеек, сделать кнопку с Reset
-    }
-  }
-
-  isWinner(index, currentCellType, cells) {
-    const columnIndex = this.getColumnIndex(index, axisElementsNumber);
-    const rowIndex = this.getRowIndex(index, columnIndex, axisElementsNumber);
-    const columnElementsIndexes = this.getColumnElementsIndexes(columnIndex, axisElementsNumber);
-    const rowElementsIndexes = this.getRowElementsIndexes(rowIndex, axisElementsNumber);
-    let isWinnerOnRlDiagonal;
-    let isWinnerOnLrDiagonal;
-
-    const isWinnerOnColumn = columnElementsIndexes.every(index => {
-      return cells[index] === currentCellType;
-    });
-
-    const isWinnerOnRow = rowElementsIndexes.every(index => {
-      return cells[index] === currentCellType;
-    });
-
-    if (rlDiagonal.includes(index)) {
-      isWinnerOnRlDiagonal = rlDiagonal.every(index => {
-        return cells[index] === currentCellType;
+    if (isWinner(index, currentCellType, cells)) {
+      this.setState({
+        winner: currentCellType,
+        showWinningMessage: 'show',
       });
     }
-
-    if (lrDiagonal.includes(index)) {
-      isWinnerOnLrDiagonal = lrDiagonal.every(index => {
-        return cells[index] === currentCellType;
-      });
-    }
-
-    return isWinnerOnColumn || isWinnerOnRow || isWinnerOnLrDiagonal || isWinnerOnRlDiagonal;
   }
-
-  getColumnIndex = (elementIndex, axisElementsNumber) => elementIndex % axisElementsNumber;
-  getRowIndex = (elementIndex, columnIndex, axisElementsNumber) => (elementIndex - columnIndex) / axisElementsNumber;
-  getRowElementsIndexes = (rowIndex, axisElementsNumber) => Array.from([...Array(axisElementsNumber).keys()], x => rowIndex * axisElementsNumber + x);
-  getColumnElementsIndexes = (columnIndex, axisElementsNumber) => Array.from([...Array(axisElementsNumber).keys()], x => columnIndex + axisElementsNumber * x);
 
   resetBoard() {
     this.setState({
       cells: Array(9).fill(null),
       cellType: 'x',
+      winner: null,
+      showWinningMessage: null,
     });
-  }
-
-  renderCell(index) {
-    return (
-        <Cell
-        value={ this.state.cells[index] }
-        onClick={ () => this.handleClick(index) }
-        />
-    );
-  }
-
-  renderWinningMessage() {
-    return (
-        <WinningMessage
-            showWinningMessage={ this.state.showWinningMessage }
-        />
-    );
-  }
+  };
 
   render() {
     return (
-        <div className={`board ${ this.state.cellType }`}>
-            {this.renderCell(0)}
-            {this.renderCell(1)}
-            {this.renderCell(2)}
-            {this.renderCell(3)}
-            {this.renderCell(4)}
-            {this.renderCell(5)}
-            {this.renderCell(6)}
-            {this.renderCell(7)}
-            {this.renderCell(8)}
+        <div className="game">
+          <Board
+              cells={this.state.cells}
+              onClick={index => this.handleClick(index)}
+              cellType={this.state.cellType}
+          />
+          <WinningMessage
+              winner={this.state.winner}
+              showWinningMessage={this.state.showWinningMessage}
+              onClick={() => this.resetBoard()}
+          />
         </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-            <Board />
     );
   }
 }
@@ -141,4 +109,44 @@ class Game extends React.Component {
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(<Game/>);
+
+const axisElementsNumber = 3;
+const lrDiagonal = Array.from([...Array(axisElementsNumber).keys()], x => x * (axisElementsNumber + 1));
+const rlDiagonal = Array.from([...Array(axisElementsNumber).keys()], x => (axisElementsNumber - 1) * (x + 1));
+
+function isWinner(index, currentCellType, cells) {
+  const columnIndex = getColumnIndex(index, axisElementsNumber);
+  const rowIndex = getRowIndex(index, columnIndex, axisElementsNumber);
+  const columnElementsIndexes = getColumnElementsIndexes(columnIndex, axisElementsNumber);
+  const rowElementsIndexes = getRowElementsIndexes(rowIndex, axisElementsNumber);
+  let isWinnerOnRlDiagonal;
+  let isWinnerOnLrDiagonal;
+
+  const isWinnerOnColumn = columnElementsIndexes.every(index => {
+    return cells[index] === currentCellType;
+  });
+
+  const isWinnerOnRow = rowElementsIndexes.every(index => {
+    return cells[index] === currentCellType;
+  });
+
+  if (rlDiagonal.includes(index)) {
+    isWinnerOnRlDiagonal = rlDiagonal.every(index => {
+      return cells[index] === currentCellType;
+    });
+  }
+
+  if (lrDiagonal.includes(index)) {
+    isWinnerOnLrDiagonal = lrDiagonal.every(index => {
+      return cells[index] === currentCellType;
+    });
+  }
+
+  return isWinnerOnColumn || isWinnerOnRow || isWinnerOnLrDiagonal || isWinnerOnRlDiagonal;
+}
+
+const getColumnIndex = (elementIndex, axisElementsNumber) => elementIndex % axisElementsNumber;
+const getRowIndex = (elementIndex, columnIndex, axisElementsNumber) => (elementIndex - columnIndex) / axisElementsNumber;
+const getRowElementsIndexes = (rowIndex, axisElementsNumber) => Array.from([...Array(axisElementsNumber).keys()], x => rowIndex * axisElementsNumber + x);
+const getColumnElementsIndexes = (columnIndex, axisElementsNumber) => Array.from([...Array(axisElementsNumber).keys()], x => columnIndex + axisElementsNumber * x);
